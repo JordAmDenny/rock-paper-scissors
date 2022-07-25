d8414439ea916ea54c8761a5001b34db6aabcf76
Jordan Denning <105526920+JordAmDenny@users.noreply.github.com>
2022-07-25 11:25:30 -0500

    quick first attempt creating playGame function

--- java.js
+++ java.js
@@ -3,3 +3,15 @@ function getComputerChoice {
   let randomChoice = Math.floor(Math.random() * choices.length);
   return randomChoice();
   }
+
+const computerSelection = getComputerChoice()
+
+function playGame (playerSelection, computerSelection) {
+  if (playerSelction == 'Rock' && computerselction == 'Scissors' || playerSelction == 'Paper' && computerselction == 'Rock' || playerSelction == 'Scissors' && computerselction == 'Paper') {
+    return "You Win!"};
+  else if (playerSelction == 'Rock' && computerselction == 'Paper' || playerSelction == 'Paper' && computerselction == 'Scissors' || playerSelction == 'Scissors' && computerselction == 'Rock') {
+    return "You Lose!"};
+  else {
+    return "Draw"
+  }
+}
