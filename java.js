5dc7e37634a743cf8f03c9d830dbdac51b5eeec1
JordAmDenny <jordandenning@hotmail.com>
2022-07-28 16:12:17 -0500

    Added score logging to half of game function/not tested

--- java.js
+++ java.js
@@ -1,4 +1,6 @@
 const choices = ["rock", "paper", "scissors"];
+let playerScore = 0
+let comScore = 0
 
 function getComputerChoice() {
     let random = choices[Math.floor(Math.random() * choices.length)];
@@ -14,10 +16,25 @@ function playGame (playerSelection, computerSelection) {
   } else if ((playerSelection === "rock" && computerSelection === "scissors") ||
             (playerSelection === "paper" && computerSelection === "rock") || 
             (playerSelection === "scissors" && computerSelection === "paper")) 
-  { return "You Win!"
+  { playerScore += 1
+    console.log("You Win, you have " +  playerScore + "points")
+    if (playerScore == 5) {
+      console.log("You take the best of 5")
+    }
+
   } else    (playerSelection === "rock" && computerSelection === "paper") ||
            (playerSelection === "paper" && computerSelection === "scissors") || 
             (playerSelection === "scissors" && computerSelection === "rock") 
   { return "You Lose!"
  }
 }
+
+
+//function game(playGame()) {
+  //let playerScore = 0
+  //let comScore = 0
+
+  //for (let i = 0; i < 5; i++)
+  //if 
+
+//}
